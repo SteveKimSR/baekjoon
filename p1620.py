dc38ef2b3bb442de0595672d0728ce80aacd34cf
SteveKimSR <kimsr96@naver.com>
2021-01-30 13:57:31 +0900

    add remark

--- p1620.py
+++ p1620.py
@@ -1,19 +1,19 @@
 import sys
 
-n, p = map(int, sys.stdin.readline().split())
-l = []
-d = {}
-for _ in range(n):
-    name = sys.stdin.readline().strip()
-    l.append(name)
-    d[name] = len(l)
-
-for _ in range(p):
-    q = input()
-    if q.isalpha():
-        print(d[q])
-    else:
-        print(l[int(q)-1])
+# n, p = map(int, sys.stdin.readline().split())
+# l = []
+# d = {}
+# for _ in range(n):
+#     name = sys.stdin.readline().strip()
+#     l.append(name)
+#     d[name] = len(l)
+#
+# for _ in range(p):
+#     q = input()
+#     if q.isalpha():
+#         print(d[q])
+#     else:
+#         print(l[int(q)-1])
 
 input = sys.stdin.readline
 
